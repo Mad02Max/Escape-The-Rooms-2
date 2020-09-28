31145d24577385304c5f08f2d5a0c8e2b4289db9
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2020-09-28 11:04:47 +0200

    You can move, somewhat

--- Escape-The-Rooms-Game/Assets/Code/Movement/Movement.cs
+++ Escape-The-Rooms-Game/Assets/Code/Movement/Movement.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class Movement : MonoBehaviour
 {
+    float speed = 3f;
+    private Rigidbody rb3d;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb3d = GetComponent <Rigidbody>();
+
     }
+ 
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb3d.AddForce(new Vector3(100, 0, 0));
+        }
+
+
+
     }
 }
