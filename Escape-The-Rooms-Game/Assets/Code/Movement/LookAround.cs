0573e33ef727ce571ac7afc70cfc3f354eda30b6
Mad02Max <71633594+Mad02Max@users.noreply.github.com>
2020-09-30 13:42:38 +0200

    Look Around
    Gjort så vi kan titta runt i scenen skall läggas till vid tillfälle

--- Escape-The-Rooms-Game/Assets/Code/Movement/LookAround.cs
+++ Escape-The-Rooms-Game/Assets/Code/Movement/LookAround.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class LookAround : MonoBehaviour
 {
+
+    // speed of mouse
     public float mouseSensitivity = 100f;
 
+
+    // Make camera moving
     public Transform playerBody;
+
+    float xRotation = 0f;
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // Get input from mouse
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        // Make you look up and down
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // So it turns around
         playerBody.Rotate(Vector3.up * mouseX);
 
     }
