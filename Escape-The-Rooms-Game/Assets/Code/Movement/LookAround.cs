178a03fbf3c2b266d11918ad39b364a6983d7df9
Mad02Max <71633594+Mad02Max@users.noreply.github.com>
2020-10-07 08:27:41 +0200

    Update LookAround.cs

--- Escape-The-Rooms-Game/Assets/Code/Movement/LookAround.cs
+++ Escape-The-Rooms-Game/Assets/Code/Movement/LookAround.cs
@@ -4,38 +4,34 @@ using UnityEngine;
 
 public class LookAround : MonoBehaviour
 {
+<<<<<<< HEAD
 
     // speed of mouse
     public float mouseSensitivity = 200f;
+=======
+    public float mouseSensitivity = 100f;
+>>>>>>> parent of 0573e33... Look Around
 
-
-    // Make camera moving
     public Transform playerBody;
+<<<<<<< HEAD
 
     // So it follows the x axis
     float xRotation = 0f;
+=======
+>>>>>>> parent of 0573e33... Look Around
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Get input from mouse
+
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        // Make you look up and down
-
-        xRotation -= mouseY;
-
-        xRotation = Mathf.Clamp(xRotation, -90f, 90);
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-        // So it turns around
         playerBody.Rotate(Vector3.up * mouseX);
 
     }
