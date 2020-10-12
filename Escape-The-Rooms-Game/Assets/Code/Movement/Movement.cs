ffe26300c8798d3e3933c25be26437e4f8134581
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2020-10-12 10:25:10 +0200

    Done with the comments for the first script

--- Escape-The-Rooms-Game/Assets/Code/Movement/Movement.cs
+++ Escape-The-Rooms-Game/Assets/Code/Movement/Movement.cs
@@ -5,22 +5,26 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class Movement : MonoBehaviour
 {
-    //We are setting a speed to easier 
+    //We are setting a speed to more easily change it if needed. We are also setting a reverse speed for the same reason.
     public float speed = 3f;
-
     public float negativSpeed = -3f;
+    
+    //We are making the rigidbody a objekt to use it.
     private Rigidbody rb3d;
 
 
 
-    // Start is called before the first frame update
+    
+    //We are doing so the rigidbody objekt in visual studio is interacting with the rigidbody of the unity objekt.
     void Start()
     {
         rb3d = GetComponent <Rigidbody>();
     }
  
 
-    // Update is called once per frame
+    //We are using "AddForce" to change the possition of the player. 
+    //Here we are using "speed" and "negativeSpeed".
+    //"Time.deltaTime" makes it so that the speed is the same across diffrent computers.
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
