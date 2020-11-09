60b47044329a3aba892a85ef9d31910bfc8ca25a
Mad02Max <71633594+Mad02Max@users.noreply.github.com>
2020-11-09 11:50:02 +0100

    Interaction click
    Du kan klicka så en ish halva delen är klar

---
+++ Escape-The-Rooms-Game/Assets/Code/Interaction/InputHandeler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace VHS
+{
+    public class InputHandeler : MonoBehaviour
+    {
+
+        #region Data
+
+        [Space, Header("Input Data")]
+        [SerializeField] private InteractionInputData interactionInputData = null;
+
+        #endregion
+
+        private void Start()
+        {
+            interactionInputData.Reset();
+        }
+        public void Update()
+        {
+            GetInteractionInputData();
+        }
+
+        #region Custom Methods
+        void GetInteractionInputData()
+        {
+            interactionInputData.interactedClicked = Input.GetKeyDown(KeyCode.E);
+
+            interactionInputData.interactedRealease = Input.GetKeyUp(KeyCode.E);
+            
+        }
+        #endregion 
+
+    }
+}
---
+++ Escape-The-Rooms-Game/Assets/Code/Interaction/InteractionInputData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VHS
+{
+    [CreateAssetMenu(fileName = "Interaction", menuName = "InteractionSST/InputData")]
+    public class InteractionInputData : ScriptableObject
+    {
+
+        private bool p_interactedClicked;
+
+        private bool p_interactedRelease;
+
+        public bool interactedClicked
+        {
+            get => p_interactedClicked;
+
+            set => p_interactedClicked = value;
+
+        }
+
+        public bool interactedRealease
+        {
+            get => p_interactedRelease;
+
+            set => p_interactedRelease = value;
+        }
+
+
+        public void Reset()
+        {
+            p_interactedClicked = false;
+
+            p_interactedRelease = false;
+        }
+
+    }
+}
+
+
