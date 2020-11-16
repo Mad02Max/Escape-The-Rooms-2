0abe3b5f30b2895702acec5f3fd7537dbe47a037
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2020-11-16 11:16:01 +0100

    The puzzle piecea collide and add 1 to a counter, they also no longer bounce once they hit the puzzle

---
+++ Escape-The-Rooms-Game/Assets/Code/PuzzlePieces/RigidbodyScriptForCubes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyScriptForCubes : MonoBehaviour
+{
+
+    private Rigidbody rb3dn2;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb3dn2 = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            rb3dn2.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+
+
+
+
+}
