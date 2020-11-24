d2133a53e1052743b3e22d08c8b1fd825d889c88
daibito <71633979+daibito@users.noreply.github.com>
2020-11-24 11:20:30 +0100

    things and cameras

---
+++ Escape-The-Rooms-Game/Assets/Code/Movement/CammeraSwitching.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CammeraSwitching : MonoBehaviour
+{
+    bool FirstPersoneCam;
+    bool PuzzleCamra;
+    public Camera charCam;
+    public Camera puzzCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirstPersoneCam = true;
+        PuzzleCamra = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* if (FirstPersoneCam == true)
+        {
+            charCam.enabled = true;
+            puzzCam.enabled = false;
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                FirstPersoneCam = false;
+                PuzzleCamra = true;
+            }
+        }
+
+        if (PuzzleCamra == true)
+        {
+            charCam.enabled = false;
+            puzzCam.enabled = true;
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                FirstPersoneCam = true;
+                PuzzleCamra = false;
+            }
+        }*/
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            if (FirstPersoneCam == false)
+            {
+                puzzCam.enabled = false;
+                charCam.enabled = true;
+            }
+            if (FirstPersoneCam == true)
+            {
+                puzzCam.enabled = true;
+                charCam.enabled = false;
+            }
+            
+          
+        }
+
+        
+    }
+}
