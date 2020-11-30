2b6ab769a2821e0cb19d40f82ffa86831b70b1f5
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2020-11-30 10:44:04 +0100

    Gjorde så att man kan röra pussel bitarna

--- Escape-The-Rooms-Game/Assets/Code/PuzzlePieces/MovePieces.cs
+++ Escape-The-Rooms-Game/Assets/Code/PuzzlePieces/MovePieces.cs
@@ -17,22 +17,51 @@ public class MovePieces : MonoBehaviour
 
     public void Awake()
     {
-        movePiece = true;
+        movePiece = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (movePiece == true)
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-
-
-
-
-
+            transform.position = transform.position + new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = transform.position + new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = transform.position + new Vector3(0, 0, -1);
         }
 
+        /*if (movePiece == true)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position = new Vector4(-1, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position = new Vector4(1, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position = new Vector4(0, 0, 1);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position = new Vector4(0, 0, -1);
+            }
 
 
+
+
+        }*/
     }
 }
