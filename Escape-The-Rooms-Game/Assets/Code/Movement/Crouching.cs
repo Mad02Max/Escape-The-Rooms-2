c5e8b5862fa1bcb0e05db53ecb1363c3431a1eb2
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2020-12-14 00:58:29 +0100

    added crouching

---
+++ Escape-The-Rooms-Game/Assets/Code/Movement/Crouching.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouching : MonoBehaviour
+{
+    public bool crouch;
+
+    void Start()
+    {
+        crouch = true;
+    }
+
+
+    void Update()
+    { 
+        if (crouch == true)
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                transform.position = transform.position + new Vector3(0, -20, 0);
+                crouch = false;
+            }
+        }
+        if (crouch == false)
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                transform.position = transform.position + new Vector3(0, 20, 0);
+                crouch = true;
+            }
+        }
+    }
+}
