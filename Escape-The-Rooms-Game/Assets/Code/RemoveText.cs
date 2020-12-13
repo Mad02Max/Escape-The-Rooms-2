ef5675b68bbb4d139fd4bc0c520fde7cd5bd9d19
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2020-12-14 00:36:55 +0100

    added a control tutorial

---
+++ Escape-The-Rooms-Game/Assets/Code/RemoveText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RemoveText : MonoBehaviour
+{
+    public Text tutorial1;
+    public Text tutorial2;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            tutorial1.enabled = false;
+            tutorial2.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            tutorial1.enabled = true;
+            tutorial2.enabled = true;
+        }
+    }
+}
