d0b9aeadc32eb6b2c96ca2c2d67522cf6fe48599
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2020-12-13 23:35:05 +0100

    added MainMenu, GameOver, buttons to change scene and exit

---
+++ Escape-The-Rooms-Game/Assets/Code/Menu/Play_game_button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play_game_button : MonoBehaviour
+{
+    public void ClickButton()
+    {
+        SceneManager.LoadScene(sceneName: "Game 1");
+    }
+}
