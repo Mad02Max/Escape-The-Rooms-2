72ed9c6666b23f4103b67ffd438f201da5e863a7
Mad02Max <71633594+Mad02Max@users.noreply.github.com>
2020-12-14 14:14:30 +0100

    open puzzle script works

--- Escape-The-Rooms-Game/Assets/Code/Interaction/OpenPuzzel.cs
+++ Escape-The-Rooms-Game/Assets/Code/Interaction/OpenPuzzel.cs
@@ -12,11 +12,19 @@ namespace VHS
         public Camera characterCam;
         public Camera puzzleCam;
 
+        public GameObject interactionUI;
+
+        public GameObject crosshair;
+
         //Sets the cameras to what they should start as.
         void Awake()
         {
             characterCam.enabled = true;
             puzzleCam.enabled = false;
+
+            crosshair.SetActive(true);
+
+            interactionUI.SetActive(true);
 
         }
 
@@ -25,25 +33,56 @@ namespace VHS
         public override void OnInteract()
         {
 
-
             base.OnInteract();
 
-            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled;
-            GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;
+            characterCam.enabled = false;
 
-            
+            puzzleCam.enabled = true;
+
+
+            //GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled;
+            //GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;
+
+
             //Gjord av Caleb
             //Gör så att man bara kan röra karaktären, kameran, pussel cursor, pussel bitar, och "croucha" när man ska kunna
-            GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>().enabled = !GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>().enabled;
-            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<LookAround>().enabled = !GameObject.FindGameObjectWithTag("MainCamera").GetComponent<LookAround>().enabled;
-            GameObject.FindGameObjectWithTag("Cursor").GetComponent<CursorMove>().enabled = !GameObject.FindGameObjectWithTag("Cursor").GetComponent<CursorMove>().enabled;
-            GameObject.FindGameObjectWithTag("crouchCam").GetComponent<Crouching>().enabled = !GameObject.FindGameObjectWithTag("crouchCam").GetComponent<Crouching>().enabled;
-            GameObject.FindGameObjectWithTag("1").GetComponent<Box1>().enabled = !GameObject.FindGameObjectWithTag("1").GetComponent<Box1>().enabled;
-            GameObject.FindGameObjectWithTag("2").GetComponent<Box1>().enabled = !GameObject.FindGameObjectWithTag("2").GetComponent<Box1>().enabled;
-            GameObject.FindGameObjectWithTag("4").GetComponent<Box1>().enabled = !GameObject.FindGameObjectWithTag("4").GetComponent<Box1>().enabled;
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>().enabled = false;
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<LookAround>().enabled = false;
+            GameObject.FindGameObjectWithTag("Cursor").GetComponent<CursorMove>().enabled = false;
+            GameObject.FindGameObjectWithTag("crouchCam").GetComponent<Crouching>().enabled = false;
+            GameObject.FindGameObjectWithTag("1").GetComponent<Box1>().enabled = false;
+            GameObject.FindGameObjectWithTag("2").GetComponent<Box1>().enabled = false;
+            GameObject.FindGameObjectWithTag("4").GetComponent<Box1>().enabled = false;
             //Slutet av gjort av Caleb
 
+            crosshair.SetActive(false);
 
+            interactionUI.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+
+                characterCam.enabled = true;
+
+                puzzleCam.enabled = false;
+
+                GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>().enabled = true;
+                GameObject.FindGameObjectWithTag("MainCamera").GetComponent<LookAround>().enabled = true;
+                GameObject.FindGameObjectWithTag("Cursor").GetComponent<CursorMove>().enabled = true;
+                GameObject.FindGameObjectWithTag("crouchCam").GetComponent<Crouching>().enabled = true;
+                GameObject.FindGameObjectWithTag("1").GetComponent<Box1>().enabled = true;
+                GameObject.FindGameObjectWithTag("2").GetComponent<Box1>().enabled = true;
+                GameObject.FindGameObjectWithTag("4").GetComponent<Box1>().enabled = true;
+
+
+                crosshair.SetActive(true);
+
+                interactionUI.SetActive(true);
+
+            }
         }
 
     }
