b5712c2a7bdf5e536136260ef08cfc3f348bc8ae
Mad02Max <71633594+Mad02Max@users.noreply.github.com>
2020-12-14 11:26:51 +0100

    .

--- Escape-The-Rooms-Game/Assets/Code/Timer/Timer.cs
+++ Escape-The-Rooms-Game/Assets/Code/Timer/Timer.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 //All done by Caleb
 
 public class Timer : MonoBehaviour
 {
+<<<<<<< HEAD
     //A float for the timer so that it can be counted and showed properly.
     //TimerText is the text that shows the timer.
     //TimerDecider is the int that is randomised, then chooses the lenght of the timer.
     public float timer;
     public Text timertext;
+=======
+    //Starts the timer when the game starts.
+    public int timer;
+>>>>>>> parent of 6d65862... made the timer a float, also made it so that one can see the timer on the canvas
     public int timerDecider;
 
     //After entering the game(past the main menu) the int "timerDecider" is randomised between 1 and 100
@@ -50,20 +54,23 @@ public class Timer : MonoBehaviour
             }
             
         }
+
+        StartCoroutine(Waiter());
     }
 
+<<<<<<< HEAD
     //In void update, I make it so that the timer is lowered by one second,I do this by using -1*Time.deltaTime
     //That makes it so that the time that is removed from the timer is always one second.
     //When the timer is at 0, the scene is switched and the cursor is activated.
     void Update()
+=======
+    //This is the timer
+    IEnumerator Waiter()
+>>>>>>> parent of 6d65862... made the timer a float, also made it so that one can see the timer on the canvas
     {
-        timer = timer - 1 * Time.deltaTime;
-        timertext.text = "timer = " + timer;
-
-        if (timer <= 0)
-        {
-            SceneManager.LoadScene(sceneName: "GameOver");
-            Cursor.lockState = CursorLockMode.None;
-        }
+        yield return new WaitForSeconds(timer);
+        SceneManager.LoadScene(sceneName: "GameOver");
+        Cursor.lockState = CursorLockMode.None;
     }
+
 }
