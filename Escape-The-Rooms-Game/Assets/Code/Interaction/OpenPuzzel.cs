eb6101f77ac461ea52c47bb8cc47731aae029454
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2020-12-14 14:34:21 +0100

    changed variables in open puzzle

--- Escape-The-Rooms-Game/Assets/Code/Interaction/OpenPuzzel.cs
+++ Escape-The-Rooms-Game/Assets/Code/Interaction/OpenPuzzel.cs
@@ -48,11 +48,11 @@ namespace VHS
             //Gör så att man bara kan röra karaktären, kameran, pussel cursor, pussel bitar, och "croucha" när man ska kunna
             GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>().enabled = false;
             GameObject.FindGameObjectWithTag("MainCamera").GetComponent<LookAround>().enabled = false;
-            GameObject.FindGameObjectWithTag("Cursor").GetComponent<CursorMove>().enabled = false;
+            GameObject.FindGameObjectWithTag("Cursor").GetComponent<CursorMove>().enabled = true;
             GameObject.FindGameObjectWithTag("crouchCam").GetComponent<Crouching>().enabled = false;
-            GameObject.FindGameObjectWithTag("1").GetComponent<Box1>().enabled = false;
-            GameObject.FindGameObjectWithTag("2").GetComponent<Box1>().enabled = false;
-            GameObject.FindGameObjectWithTag("4").GetComponent<Box1>().enabled = false;
+            GameObject.FindGameObjectWithTag("1").GetComponent<Box1>().enabled = true;
+            GameObject.FindGameObjectWithTag("2").GetComponent<Box1>().enabled = true;
+            GameObject.FindGameObjectWithTag("4").GetComponent<Box1>().enabled = true;
             //Slutet av gjort av Caleb
 
             crosshair.SetActive(false);
@@ -71,11 +71,11 @@ namespace VHS
 
                 GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>().enabled = true;
                 GameObject.FindGameObjectWithTag("MainCamera").GetComponent<LookAround>().enabled = true;
-                GameObject.FindGameObjectWithTag("Cursor").GetComponent<CursorMove>().enabled = true;
+                GameObject.FindGameObjectWithTag("Cursor").GetComponent<CursorMove>().enabled = false;
                 GameObject.FindGameObjectWithTag("crouchCam").GetComponent<Crouching>().enabled = true;
-                GameObject.FindGameObjectWithTag("1").GetComponent<Box1>().enabled = true;
-                GameObject.FindGameObjectWithTag("2").GetComponent<Box1>().enabled = true;
-                GameObject.FindGameObjectWithTag("4").GetComponent<Box1>().enabled = true;
+                GameObject.FindGameObjectWithTag("1").GetComponent<Box1>().enabled = false;
+                GameObject.FindGameObjectWithTag("2").GetComponent<Box1>().enabled = false;
+                GameObject.FindGameObjectWithTag("4").GetComponent<Box1>().enabled = false;
 
 
                 crosshair.SetActive(true);
