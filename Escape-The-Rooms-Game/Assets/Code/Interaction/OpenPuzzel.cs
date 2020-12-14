f5fa5e8804473ffb54366b7a356e3a9286e9794c
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2020-12-14 11:10:59 +0100

    idk

--- Escape-The-Rooms-Game/Assets/Code/Interaction/OpenPuzzel.cs
+++ Escape-The-Rooms-Game/Assets/Code/Interaction/OpenPuzzel.cs
@@ -11,23 +11,50 @@ namespace VHS
         //Creates cameras
         public Camera characterCam;
         public Camera puzzleCam;
+<<<<<<< HEAD
+=======
+
+
+        /*[SerializeField] Camera characterCam;
+        [SerializeField] Camera puzzleCam;
+
+        [SerializeField] KeyCode[] toggleCamera;*/
+
+        //public Movement movement;
+>>>>>>> parent of 6c2fd7b... crouch cam script
 
         //Sets the cameras to what they should start as.
         void Awake()
         {
             characterCam.enabled = true;
             puzzleCam.enabled = false;
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 6c2fd7b... crouch cam script
         }
 
         //Uses the interaction script so that everything happenes when you are looking at the puzzle and pressing "E".
         //Switches the cameras and turn on and off different scripts so that they are active when they should.
         public override void OnInteract()
         {
+
+
             base.OnInteract();
 
+
+
             GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled;
+<<<<<<< HEAD
             GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;            
+=======
+            GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;
+
+
+            //characterCam.enabled = false;
+
+            //puzzleCam.enabled = true;
+>>>>>>> parent of 6c2fd7b... crouch cam script
 
             //Gjord av Caleb
             //Gör så att man bara kan röra karaktären, kameran, pussel cursor, pussel bitar, och "croucha" när man ska kunna
@@ -39,6 +66,10 @@ namespace VHS
             GameObject.FindGameObjectWithTag("2").GetComponent<Box1>().enabled = !GameObject.FindGameObjectWithTag("2").GetComponent<Box1>().enabled;
             GameObject.FindGameObjectWithTag("4").GetComponent<Box1>().enabled = !GameObject.FindGameObjectWithTag("4").GetComponent<Box1>().enabled;
             //Slutet av gjort av Caleb
+<<<<<<< HEAD
         }        
+=======
+        }
+>>>>>>> parent of 6c2fd7b... crouch cam script
     }
 }
