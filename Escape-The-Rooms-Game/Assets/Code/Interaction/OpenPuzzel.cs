b78ea40016b3141c85c3cabc9d5316b5ac7b5768
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2020-12-14 11:46:56 +0100

    cleaned open puzzle

--- Escape-The-Rooms-Game/Assets/Code/Interaction/OpenPuzzel.cs
+++ Escape-The-Rooms-Game/Assets/Code/Interaction/OpenPuzzel.cs
@@ -11,27 +11,13 @@ namespace VHS
         //Creates cameras
         public Camera characterCam;
         public Camera puzzleCam;
-<<<<<<< HEAD
-=======
-
-
-        /*[SerializeField] Camera characterCam;
-        [SerializeField] Camera puzzleCam;
-
-        [SerializeField] KeyCode[] toggleCamera;*/
-
-        //public Movement movement;
->>>>>>> parent of 6c2fd7b... crouch cam script
 
         //Sets the cameras to what they should start as.
         void Awake()
         {
             characterCam.enabled = true;
             puzzleCam.enabled = false;
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 6c2fd7b... crouch cam script
         }
 
         //Uses the interaction script so that everything happenes when you are looking at the puzzle and pressing "E".
@@ -42,39 +28,27 @@ namespace VHS
 
             base.OnInteract();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 
             GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled;
-<<<<<<< HEAD
-            GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;            
-=======
+            GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;
             GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;
 
 
             //characterCam.enabled = false;
 
             //puzzleCam.enabled = true;
->>>>>>> parent of 6c2fd7b... crouch cam script
-=======
+
             GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled;
-            GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;            
->>>>>>> parent of 5fde4d5... open puzzle stuff + game build
-=======
+            GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;
             GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled;
-            GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;            
->>>>>>> parent of 5fde4d5... open puzzle stuff + game build
-=======
+            GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;
             GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled;
-            GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;            
->>>>>>> parent of 5fde4d5... open puzzle stuff + game build
-=======
+            GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;
             GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled;
-            GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;            
->>>>>>> parent of 5fde4d5... open puzzle stuff + game build
+            GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled = !GameObject.FindGameObjectWithTag("PuzzleCamera").GetComponent<Camera>().enabled;
+
 
             //Gjord av Caleb
             //Gör så att man bara kan röra karaktären, kameran, pussel cursor, pussel bitar, och "croucha" när man ska kunna
@@ -86,26 +60,8 @@ namespace VHS
             GameObject.FindGameObjectWithTag("2").GetComponent<Box1>().enabled = !GameObject.FindGameObjectWithTag("2").GetComponent<Box1>().enabled;
             GameObject.FindGameObjectWithTag("4").GetComponent<Box1>().enabled = !GameObject.FindGameObjectWithTag("4").GetComponent<Box1>().enabled;
             //Slutet av gjort av Caleb
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        }        
-=======
+
         }
->>>>>>> parent of 6c2fd7b... crouch cam script
-=======
-        }        
->>>>>>> parent of 5fde4d5... open puzzle stuff + game build
-=======
-        }        
->>>>>>> parent of 5fde4d5... open puzzle stuff + game build
-=======
-        }        
->>>>>>> parent of 5fde4d5... open puzzle stuff + game build
-=======
-        }        
->>>>>>> parent of 5fde4d5... open puzzle stuff + game build
+
     }
 }
