20f589f470f15f49352aba4ea541466d17bcd7e4
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2020-12-14 09:18:01 +0100

    added comment

--- Escape-The-Rooms-Game/Assets/Code/Canvas/CanvasInput.cs
+++ Escape-The-Rooms-Game/Assets/Code/Canvas/CanvasInput.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Made by Caleb, copied from Max InventoryInput
+
 public class CanvasInput : MonoBehaviour
 {
     [SerializeField] GameObject tutorialObject1;
