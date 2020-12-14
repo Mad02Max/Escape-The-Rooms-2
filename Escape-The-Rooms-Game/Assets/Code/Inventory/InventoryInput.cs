a48bff8e64551abdb6145c4a00d73b3ff69cf4fb
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2020-12-14 09:05:23 +0100

    improved tutorial text appering and dissapering

--- Escape-The-Rooms-Game/Assets/Code/Inventory/InventoryInput.cs
+++ Escape-The-Rooms-Game/Assets/Code/Inventory/InventoryInput.cs
@@ -6,8 +6,11 @@ public class InventoryInput : MonoBehaviour
 {
 
     [SerializeField] GameObject inventoryObject;
+    [SerializeField] GameObject tutorialObject1;
+    [SerializeField] GameObject tutorialObject2;
 
     [SerializeField] KeyCode[] toggleInventory;
+    [SerializeField] KeyCode[] toggleTutorial;
     // Update is called once per frame
     void Update()
     {
@@ -17,6 +20,15 @@ public class InventoryInput : MonoBehaviour
             if (Input.GetKeyDown(toggleInventory[i]))
             {
                 inventoryObject.SetActive(!inventoryObject.activeSelf);
+                break;
+            }
+        }
+        for (int i = 0; i < toggleTutorial.Length; i++)
+        {
+            if (Input.GetKeyDown(toggleTutorial[i]))
+            {
+                tutorialObject1.SetActive(!tutorialObject1.activeSelf);
+                tutorialObject2.SetActive(!tutorialObject2.activeSelf);
                 break;
             }
         }
