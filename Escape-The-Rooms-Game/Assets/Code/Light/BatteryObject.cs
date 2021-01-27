a8f0c99fa8662c31ab02a5b6d3068fb25e2f439d
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2021-01-27 13:35:35 +0100

    Battery works

---
+++ Escape-The-Rooms-Game/Assets/Code/Light/BatteryObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VHS
+{
+    public class BatteryObject : InteractableBase
+    {
+
+        public GameObject battery;
+
+
+        public override void OnInteract()
+        {
+            base.OnInteract();
+
+            GameObject.FindGameObjectWithTag("Flashlight").GetComponent<Battery>().batteryLife = GameObject.FindGameObjectWithTag("Flashlight").GetComponent<Battery>().batteryLife + 10;
+
+            Destroy(gameObject);
+        }
+    }
+}
