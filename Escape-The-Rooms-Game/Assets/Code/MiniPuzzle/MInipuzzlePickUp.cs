970e6646f67ebe1d9bb18b16da28ad9f381f2cd8
Mad02Max <71633594+Mad02Max@users.noreply.github.com>
2021-01-27 14:48:33 +0100

    edited name on script

--- Escape-The-Rooms-Game/Assets/Code/MiniPuzzle/MInipuzzlePickUp.cs
+++ Escape-The-Rooms-Game/Assets/Code/MiniPuzzle/MInipuzzlePickUp.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace VHS
 {
-    public class MInipuzzlePickUp : MonoBehaviour
+    public class MinipuzzlePickUp : InteractableBase
     {
-        
+
+        public bool key = false;
+
+        public override void OnInteract()
+        {
+            base.OnInteract();
+
+            // Where the object can spawn
+            //Instantiate(piece, pos.transform.position, Quaternion.identity);
+
+
+            // Destroys object
+            Destroy(gameObject);
+
+        }
+
     }
 }
