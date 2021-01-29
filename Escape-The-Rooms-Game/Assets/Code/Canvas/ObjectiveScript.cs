d8fd4d537c9638d938c375cbaedcd96484a4169a
Necr0Nomicon <71634334+Necr0Nomicon@users.noreply.github.com>
2021-01-29 10:21:01 +0100

    comments

--- Escape-The-Rooms-Game/Assets/Code/Canvas/ObjectiveScript.cs
+++ Escape-The-Rooms-Game/Assets/Code/Canvas/ObjectiveScript.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//By Caleb
+
 public class ObjectiveScript : MonoBehaviour
 {
+    //Connects text to unity.
     public Text objectiveText;
 
+    //Starts timer.
     public void Start()
     {
         StartCoroutine(Waiter());
     }
 
-
+    //Destroy text after timer.
     IEnumerator Waiter()
     {
         yield return new WaitForSeconds(30);
