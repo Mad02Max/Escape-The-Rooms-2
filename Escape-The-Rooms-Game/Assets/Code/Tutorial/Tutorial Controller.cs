0958ce00b711616113d98f7563e7c114646a0b22
Mad02Max <71633594+Mad02Max@users.noreply.github.com>
2021-03-15 11:08:59 +0100

    Tutorial visuals

---
+++ Escape-The-Rooms-Game/Assets/Code/Tutorial/Tutorial Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+
+    public bool w = false;
+
+    public bool s = false;
+
+    public bool d = false;
+
+    public bool a = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+
+    }
+}
