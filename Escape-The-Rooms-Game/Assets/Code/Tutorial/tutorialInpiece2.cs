32a6bceedde4e1ceca473941fbc814c04f5d4582
Mad02Max <71633594+Mad02Max@users.noreply.github.com>
2021-03-22 11:31:16 +0100

    tutorial

---
+++ Escape-The-Rooms-Game/Assets/Code/Tutorial/tutorialInpiece2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class tutorialInpiece2 : MonoBehaviour
+{
+
+    public Toggle rotateL;
+
+    public Toggle rotateR;
+
+    public GameObject tutorialBackground;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(rotateL.isOn == true && rotateR.isOn == true)
+        {
+            Destroy(tutorialBackground);
+        }
+
+    }
+}
