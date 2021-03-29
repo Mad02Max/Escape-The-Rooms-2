c90e40e91c885fa40394b9d8e2ad467b184d4cb0
Mad02Max <71633594+Mad02Max@users.noreply.github.com>
2021-03-29 11:10:37 +0200

    tutorial almost done

---
+++ Escape-The-Rooms-Game/Assets/Code/Tutorial/RotateTutorial.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RotateTutorial : MonoBehaviour
+{
+
+    public Toggle pressB;
+
+    public Toggle pressV;
+
+    public GameObject tutorialBackgorund;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (pressB.isOn == true && pressV.isOn == true)
+        {
+            tutorialBackgorund.SetActive(false);
+
+            Destroy(gameObject);
+        }
+
+    }
+}
